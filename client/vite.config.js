267b8f75fbf4441d645c5069f31aac392058c2bc
Doducnghia00 <89834732+Doducnghia00@users.noreply.github.com>
2025-11-25 09:48:23 +0700

    feat: Configure Vite development server with host, port, and HMR settings.

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
         react(),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5174,
+        hmr: {
+            protocol: 'ws',
+            host: 'localhost',
+            clientPort: 5174,
+        },
+    },
 });
